3987844e6585fa876f7d377a5e120a08b0297399
ambsenyestudi <adrifre7@hotmail.com>
2021-06-12 16:03:07 +0200

    Corner move red

---
+++ ChessKnightMove/ChessKnightMove.Domain/Pieces/Knight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChessKnightMove.Domain.Pieces
+{
+    public class Knight
+    {
+        private string position;
+
+        public Knight(string position)
+        {
+            this.position = position;
+        }
+
+        public List<string> ListPossibleMovements()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ ChessKnightMove/ChessKnightMove.Test/Pieces/KnightShould.cs
@@ -0,0 +1,16 @@
+﻿using ChessKnightMove.Domain.Pieces;
+using Xunit;
+
+namespace ChessKnightMove.Test.Pieces
+{
+    public class KnightShould
+    {
+        [Fact]
+        public void Contain_move()
+        {
+            var sut = new Knight("A1");
+            var postionList = sut.ListPossibleMovements();
+            Assert.Contains("B3", postionList);
+        }
+    }
+}
