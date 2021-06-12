23fb4a02a5d90036b02891b26fa37b1528c5cff3
ambsenyestudi <adrifre7@hotmail.com>
2021-06-12 16:32:23 +0200

    When at  second row red

--- ChessKnightMove/ChessKnightMove.Test/Pieces/KnightShould.cs
+++ ChessKnightMove/ChessKnightMove.Test/Pieces/KnightShould.cs
@@ -9,7 +9,17 @@ namespace ChessKnightMove.Test.Pieces
         [Theory]
         [InlineData("A1", "B3")]
         [InlineData("A1", "C2")]
-        public void Contain_move(string knighPositionRaw, string expected)
+        public void Contain_move_when_at_corner(string knighPositionRaw, string expected)
+        {
+            var knighPosition = Postition.FromString(knighPositionRaw);
+            var exptectedPosition = Postition.FromString(expected);
+            var sut = new Knight(knighPosition);
+            var postionList = sut.ListPossibleMovements();
+            Assert.Contains(exptectedPosition, postionList);
+        }
+        [Theory]
+        [InlineData("C2", "A1")]
+        public void Contain_move_when_at_second_row(string knighPositionRaw, string expected)
         {
             var knighPosition = Postition.FromString(knighPositionRaw);
             var exptectedPosition = Postition.FromString(expected);
